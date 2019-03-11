991df5e751e169aff4565c50a5614106854c80e1
Hugo <hugoesenv@gmail.com>
2019-03-11 15:52:08 -0300

    Finalizing CRUD for Person class

--- RestWithAspNETUdemy002/RestWithAspNETUdemy/Services/Implementattions/PersonService.cs
+++ RestWithAspNETUdemy002/RestWithAspNETUdemy/Services/Implementattions/PersonService.cs
@@ -1,68 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using RestWithAspNETUdemy.Model;
+using RestWithAspNETUdemy.Model.Context;
 using RestWithAspNETUdemy.Services.Interfaces;
 
 namespace RestWithAspNETUdemy.Services.Implementattions
 {
     public class PersonService : IPersonService
     {
-        private volatile int count;
-
+        private MySQLContext _context;
+        
+        public PersonService(MySQLContext context)
+        {
+            _context = context;
+        }
         public Person Create(Person person)
         {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
             return person;
         }
 
         public void Delete(long id)
         {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+            try
+            {
+                if (result != null)
+                    _context.Persons.Remove(result);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public List<Person> FindAll()
         {
-            List<Person> persons = new List<Person>();
-            for (int i = 0; i < 8; i++)
-            {
-                Person person = MockPerson(i);
-                persons.Add(person);
-            }
-
-            return persons;
-        }
-
-        private Person MockPerson(object i)
-        {
-            return new Person
-            {
-                Id = IncrementAndGet(),
-                FirstName = "Person Name " + i,
-                LastName = "Person LastName " + i,
-                Address = "Some Address " + i,
-                Gender = "Male"
-            };
-        }
-
-        private long IncrementAndGet()
-        {
-            return Interlocked.Increment(ref count);
+            return _context.Persons.ToList();
         }
 
         public Person FindById(long id)
         {
-            return new Person
-            {
-                Id = IncrementAndGet(),
-                FirstName = "Hugo",
-                LastName = "Ventura",
-                Address = "Florisnópolis/SC, Brasil",
-                Gender = "M"
-            };
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
         }
 
         public Person Update(Person person)
         {
+            if (!Exists(person.Id))
+                return new Person();
+
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+            try
+            {
+                _context.Entry(result).CurrentValues.SetValues(person);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
             return person;
+        }
+
+        private bool Exists(long? id)
+        {
+            return _context.Persons.Any(p => p.Id.Equals(id));
         }
     }
 }
